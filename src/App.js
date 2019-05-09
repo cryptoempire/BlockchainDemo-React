81095b085e57df6c2fc9560189f96768d8000e29
cryptoempire <ecehood@gmail.com>
2019-05-08 21:12:23 -0500

    added hash input

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   return (
     <div>
       <Roundlogo />
-      <div className='title'>Blockchain</div>
+      <div className='title'>BLOCKCHAIN</div>
       <GenesisBlock/>
       <AddCard />
       {/*{
--- src/components/GenesisBlock/GenesisBlock.js
+++ src/components/GenesisBlock/GenesisBlock.js
@@ -10,9 +10,9 @@ const GenesisBlock = () => {
         <div class="block-card">
           <div class="card_body">
             <div class="data"><div class="dataBox">DATA</div><input type="text" name="" placeholder="" value="Welcome to Blockchain Demo 2.0!" /></div>
-            <div style={{display:'flex'}}><p>PREVIOUS HASH</p> <p style={{marginLeft: '12px', color: 'green', fontSize: '10px', marginTop: '10px'}}>0</p></div>
-            <div><p>HASH</p></div>
-            <div style={{display: 'flex'}}><span style={{font: '24px'}}>GENESIS BLOCK</span><div className='time time-genesis'>on {time}</div></div>
+            <div style={{display:'flex', height: '30px'}}><p>PREVIOUS HASH</p> <p style={{marginLeft: '12px', color: 'green', fontSize: '10px', marginTop: '10px'}}>0</p></div>
+            <div style={{display: 'flex'}}><p>HASH</p><div className='ant-tag-green '>{/*hash*/}</div></div>
+            <div style={{display: 'flex', marginTop: '24px'}}><span style={{fontSize: '24px', letterSpacing: '1px'}}>GENESIS BLOCK</span><div className='time time-genesis'>on {time}</div></div>
           </div>
         </div>
       </div>
