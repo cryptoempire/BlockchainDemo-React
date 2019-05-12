63ac90f489260994d37f8abc7dc4a00391054ab3
cryptoempire <ecehood@gmail.com>
2019-05-12 02:22:04 -0500

    successfully managed to change the state on hash and previous hash, however am having trouble manipulating id due to nested state

--- src/App.js
+++ src/App.js
@@ -7,31 +7,50 @@ import './App.css';
 const sha256 = require('sha256');
 
 const initialState = {
-   blocks : [ ]
+  block: {
+    id: 'GENESIS BLOCK',
+    previousHash : 0,
+    hash : sha256(0),
+    dayCreated : new Date().toGMTString(),
+    data: '  Welcome to the Blockchain!'
+  }
 };
+
 
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      block: {
-        id: 'GENESIS BLOCK',
-        previousHash : 0,
-        hash : sha256(0),
-        dayCreated : new Date().toGMTString(),
-        data: '  Welcome to the Blockchain!'
-      }
-    }
+    this.state = initialState;
   }
 
 createBlock = () => {
 
 }
 
-onButtonClick = () => {
-  console.log('clicked');
 
+onButtonClick = () => {
+
+    console.log('clicked');
+    console.log(this.state.block.id, 'initial ID');
+    if (this.state.block.id === 'GENESIS BLOCK') {
+      this.setState(Object.assign(this.state.block, {id: 0}));
+      // this.setState({block: { ...this.state.block, id: 0}});
+    }
+
+
+    console.log(this.state.block.id, '  changed state');
+
+  this.setState({
+    block: {
+      id: this.state.block.id++,
+      previousHash : this.state.block.hash,
+      hash : sha256(this.state.block.data + this.state.block.id + this.state.block.previousHash),
+      dayCreated : new Date().toGMTString(),
+
+    }
+  })
+    console.log(this.state.block.id);
 
 }
 
