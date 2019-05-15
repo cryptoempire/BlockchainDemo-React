4ddbe8d9d08c049561faff6b853bf723278108b5
cryptoempire <ecehood@gmail.com>
2019-05-15 03:03:10 -0500

    I finally did it, I am so excited, I integrated componentDidMount() and used a ternary operator to output genesis block for first index

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 const sha256 = require('sha256');
 
 const initialState = {
-    blockId: 'GENESIS BLOCK',
+    blockId: 0,
     previousHash : 0,
     hash : sha256(Math.random()),
     dayCreated : new Date().toGMTString(),
@@ -24,50 +24,51 @@ class App extends React.Component {
     this.state = initialState;
   }
 
-componentDidMount() {
+  componentDidMount() {
+    this.setState({
+        blockId: this.state.blockId++,
+        previousHash : this.state.hash,
+        hash : sha256(this.state.data + this.state.blockId + this.state.previousHash),
+        dayCreated : new Date().toGMTString(),
+    })
 
-}
+      console.log(this.state.blockId);
+      this.setState({blocks: [...this.state.blocks, [this.state.blockId, this.state.previousHash, this.state.hash, this.state.dayCreated] ]});
+      console.log(this.state.blocks);
+  }
+
+
 
 onButtonClick = () => {
     console.log(this.state.blockId, 'initial ID');
-    this.setState({})
-
-this.setState({blocks: [...this.state.blocks, [this.state.blockId, this.state.previousHash, this.state.hash, this.state.dayCreated] ]});
 
   this.setState({
-
       blockId: this.state.blockId++,
       previousHash : this.state.hash,
       hash : sha256(this.state.data + this.state.blockId + this.state.previousHash),
       dayCreated : new Date().toGMTString(),
-
-
   })
+
     console.log(this.state.blockId);
-
-
-
-console.log(this.state.blocks);
-
+    this.setState({blocks: [...this.state.blocks, [this.state.blockId, this.state.previousHash, this.state.hash, this.state.dayCreated] ]});
+    console.log(this.state.blocks);
 }
 
 
   render() {
     const {blockId, previousHash, hash, dayCreated, data} = this.state;
-console.log(hash);
-console.log(blockId);
+
   return (
 
     <div>
       <Roundlogo />
       <div className='title'>BLOCKCHAIN</div>
 
-      <GenesisBlock
+      {/* {<GenesisBlock
         blockId={blockId}
-
         hash={hash}
         dayCreated={dayCreated}
-        data={data} />
+      data={data} />} */}
       {
         this.state.blocks.map((blockMap, index) => {
           return (
@@ -77,16 +78,20 @@ console.log(blockId);
                   <div className="data"><div className="dataBox">DATA</div><input type="text" name="" placeholder="" defaultValue="    Welcome to Blockchain Demo 2.0!" /></div>
                   <div style={{display:'flex', height: '30px'}}><p>PREVIOUS HASH</p> <p style={{marginLeft: '12px', color: 'green', fontSize: '10px', marginTop: '10px'}}>{blockMap[1]}</p></div>
                   <div style={{display: 'flex'}}><p>HASH</p><div className='ant-tag-green'>{blockMap[2]}</div></div>
-                  <div style={{display: 'flex', marginTop: '24px'}}><span style={{fontSize: '24px', letterSpacing: '1px'}}>{`BLOCK # ${index + 1}`}</span><div className='time time-genesis'>on {blockMap[3]}</div></div>
+                  <div style={{display: 'flex', marginTop: '24px'}}><span style={{fontSize: '24px', letterSpacing: '1px'}}>{index === 0 ? 'GENESIS BLOCK' : `BLOCK # ${index}`}</span><div className='time time-genesis'>on {blockMap[3]}</div></div>
                 </div>
               </div>
             </div>
           )
         })
       }
+      {console.log(this.state.blocksS)}
+      {console.log(hash, 'current hash')}
 
+      {console.log(blockId, 'blockId')}
+      {console.log(previousHash, 'previousHash')}
       <AddCard onButtonClick={this.onButtonClick} />
-
+      
     </div>
   );
 }
